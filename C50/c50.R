02f6f9e5ee34399aedf5f62a7deb331858d836ec
xgerrmann <xander@gerrmann.nl>
2016-06-26 18:23:55 +0200

    changes on c50 to cope with other dataset

--- C50/c50.R
+++ C50/c50.R
@@ -1,74 +1,63 @@
 #! /usr/bin/Rscript
 
-#url <- "https://archive.ics.uci.edu/ml/machine-learning-databases/credit-screening/crx.data""
-#crx <- read.table( file="./crx.dat", header=FALSE, sep="," )
-#url <- "https://archive.ics.uci.edu/ml/machine-learning-databases/credit-screening/crx.data"
-
 library(C50)
 
-url <- '../train.dat'
+url <- '../data/train.dat'
+#url <- '../data/train_NN.dat'
 dat <- read.table( file=url, header=TRUE, sep="," )
 
-write.table( dat, "tmp.dat", quote=FALSE, sep="," )
-
-submit = FALSE
+submit = FALSE # column are not correct, fix this for NN dataset
 
 if(!submit){
-	#head( dat, 6 )
-	n_crossval = 20
+	print(tail( dat, 2 ))
+	n_crossval = 1
 	ERR		= rep(0,n_crossval)
 	ERR_C0	= rep(0,n_crossval)
 	ERR_C1	= rep(0,n_crossval)
 	
 	for(i in 1:n_crossval){
 		# shuffle the data
-		n_data	<- nrow(dat)
-		#n_data
+		n_data	= NROW(dat)
+		n_feat	= NCOL(dat[1,])-1
+		print(n_data)
+		print(n_feat)
 		n_train <- 0.95 * n_data
 		#n_train
 		dat_shuffle <- dat[sample(n_data),]
-		
-		
+
 		# split labels and real data
-		X <- dat_shuffle[,1:13]
-		y <- dat_shuffle[,14]
+		X <- dat_shuffle[,1:n_feat]
+		y <- dat_shuffle[,n_feat+1]
 		
 		# split test and trainset
-		trainX <- X[1:n_train,]
-		trainy <- y[1:n_train]
-		testX <- X[(n_train+1):n_data,]
-		testy <- y[(n_train+1):n_data]
+		trainX	<- X[1:n_train,]
+		trainy	<- y[1:n_train]
+		testX	<- X[(n_train+1):n_data,]
+		testy	<- y[(n_train+1):n_data]
 		
 		#model <- C50::C5.0( trainX, trainy )
+		#print(paste('Dim trainX: ',dim(trainX)))
 		model <- C50::C5.0( trainX, trainy, trials=100)
 		#summary( model )
 		
 		#p <- predict( model, trainX, type="class" )
 		p <- predict( model, testX, type="class" )
-		#p
-		#testy
-		#length(trainy)
-		#length(testy)
-		#length(p)
-		#sum( p == trainy )/length(p)
+		
 		n_c0_test <- sum(testy=='-')
 		n_c1_test <- sum(testy=='+')
-		n_c0_test
-		n_c1_test
 		correct_total	<- sum( p == testy )/length(p)
-		#p
-		#testy
+		
 		correct_c0		<- sum(as.integer(p[testy=='-'] == testy[testy=='-']))/n_c0_test
 		correct_c1		<- sum(as.integer(p[testy=='+'] == testy[testy=='+']))/n_c1_test
 	
 		ERR[i]		= 1-correct_total
 		ERR_C0[i]	= 1-correct_c0
 		ERR_C1[i]	= 1-correct_c1
-		#n_data
-		#n_train
-		#print(paste('Correct_total: ',1-correct_total))
-		#print(paste('Correct_c0:    ',1-correct_c0))
-		#print(paste('Correct_c1:    ',1-correct_c1))
+		n_data
+		n_train
+		print(paste('Correct_total: ',1-correct_total))
+		print(paste('Correct_c0:    ',1-correct_c0))
+		print(paste('Correct_c1:    ',1-correct_c1))
 	}
 	
 	mean_total	= mean(ERR)
@@ -80,18 +69,18 @@ if(!submit){
 	print(paste('Correct_c1 mean:    ',mean_c1))
 }else{
 	## Make submission
-	file_train <- '../train.dat'
+	file_train <- '../data/train.dat'
 	dat_train	<- read.table( file=file_train, header=TRUE, sep="," )
 	
-	file_test	<- '../test.dat'
+	file_test	<- '../data/test.dat'
 	dat_test	<- read.table( file=file_test, header=TRUE, sep="," )
+	n_feat	= NCOL(dat_train[1,])-1
 	
-	write.table( dat_train, "train_tmp.dat", quote=FALSE, sep="," )
-	write.table( dat_test, "test_tmp.dat", quote=FALSE, sep="," )
-	
-	# Split data (labels and real data)
-	data <- dat_train[,1:13]
-	labels <- dat_train[,14]
+	dat_test = dat_test[,1:n_feat]
+	print(dim(dat_test))
+	# split labels and real data
+	data	<- dat_train[,1:n_feat]
+	labels	<- dat_train[,n_feat+1]
 	
 	# Train model
 	model <- C50::C5.0( data, labels, trials=100 )
@@ -106,11 +95,11 @@ if(!submit){
 	write('Id,Prediction',fname)
 	
 	for(i in 1:length(predictions)){
-		if(predictions[i]=='-'){
-			write(paste(i,'0',sep=','),fname,append= TRUE)
-		}else{
-			write(paste(i,'1',sep=','),fname,append= TRUE)
-		}
+		#if(predictions[i]=='-'){
+		write(paste(i,predictions[i],sep=','),fname,append= TRUE)
+		#}else{
+		#	write(paste(i,'1',sep=','),fname,append= TRUE)
+		#}
 	}
 	
 	
