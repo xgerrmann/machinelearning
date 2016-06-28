3187b34e78db227d67cf1f289bfc2fde5aaabcc1
xgerrmann <xander@gerrmann.nl>
2016-06-28 16:31:23 +0200

    NN works for all features.

--- NN/NN.R
+++ NN/NN.R
@@ -11,7 +11,7 @@ submit = FALSE
 
 if(!submit){
 	#print(head( dat, 6 ))
-	n_crossval = 1
+	n_crossval = 5
 	ERR		= rep(0,n_crossval)
 	ERR_C0	= rep(0,n_crossval)
 	ERR_C1	= rep(0,n_crossval)
@@ -28,8 +28,9 @@ if(!submit){
 		X <- dat_shuffle[,1:n_feat]
 		y <- dat_shuffle[,n_feat+1]
 		# split test and trainset
-		N_FEAT = 40
+		N_FEAT = 112
 		trainX <- X[1:n_train,1:N_FEAT]
+		#trainX <- X[1:n_train,]
 		trainy <- y[1:n_train]
 		testX <- X[(n_train+1):n_data,]
 		testy <- y[(n_train+1):n_data]
@@ -42,37 +43,51 @@ if(!submit){
 		print(t)
 		print(parse(text=t))
 		#net.sqrt <- neuralnet( Output~V1 + V2, trainingdata, hidden = 10, threshold = 0.1)
-		net.sqrt <- neuralnet( eval(parse(text=t)), trainingdata, hidden = 10, threshold = 0.1)
+		hid		= c(10,10)
+		thresh	= 0.1
+		lrs		= 1
+
+		net.sqrt <- neuralnet( eval(parse(text=t)), trainingdata, hidden = hid, threshold = thresh)
 			
 		testdata <- testX[,1:N_FEAT]
-		net.results <- compute(net.sqrt, testdata) 
+		#testdata <- testX
+		net.results <- compute(net.sqrt, testdata)
+		print(net.results$net.result)
+		write.table( cbind(testy,as.data.frame(net.results$net.result)), paste("results/crossval_",i,"-",n_crossval,'_hidden_',hid,'_thresh_',thresh,'_layers_',lrs,'_feat_',N_FEAT,".dat"), quote=FALSE, sep="," )
 		#Lets display a better version of the results
+		res = as.data.frame(net.results$net.result)
 		cleanoutput <- cbind(testdata,testy,
                          as.data.frame(net.results$net.result))
-		#colnames(cleanoutput) <- c("Input1","Input2","Expected_Output","NNet_Output")
+		
 		colnames(cleanoutput) <- c(colnames(testdata),"Expected_Output","NNet_Output")
 		print(cleanoutput)
-# 		n_c0_test <- sum(testy=='-')
-# 		n_c1_test <- sum(testy=='+')
+		thresh = 0.4
+		p = res>thresh
+		print(p)
+ 		n_c0_test <- sum(testy=='0')
+ 		n_c1_test <- sum(testy=='1')
 # 		n_c0_test
 # 		n_c1_test
-# 		correct_total	<- sum( p == testy )/length(p)
-# 		
-# 		correct_c0		<- sum(as.integer(p[testy=='-'] == testy[testy=='-']))/n_c0_test
-# 		correct_c1		<- sum(as.integer(p[testy=='+'] == testy[testy=='+']))/n_c1_test
-# 	
-# 		ERR[i]		= 1-correct_total
-# 		ERR_C0[i]	= 1-correct_c0
-# 		ERR_C1[i]	= 1-correct_c1
+ 		correct_total	<- sum( p == testy )/length(p)
+
+ 		correct_c0		<- sum(as.integer(p[testy=='0'] == testy[testy=='0']))/n_c0_test
+ 		correct_c1		<- sum(as.integer(p[testy=='1'] == testy[testy=='1']))/n_c1_test
+
+ 		ERR[i]		= 1-correct_total
+ 		ERR_C0[i]	= 1-correct_c0
+ 		ERR_C1[i]	= 1-correct_c1
+ 		print(ERR[i])
+ 		print(ERR_C0[i])
+ 		print(ERR_C1[i])
 	}
 	
-#	mean_total	= mean(ERR)
-#	mean_c0		= mean(ERR_C0)
-#	mean_c1		= mean(ERR_C1)
+	mean_total	= mean(ERR)
+	mean_c0		= mean(ERR_C0)
+	mean_c1		= mean(ERR_C1)
 #	
-#	print(paste('Correct_total mean: ',mean_total))
-#	print(paste('Correct_c0 mean:    ',mean_c0))
-#	print(paste('Correct_c1 mean:    ',mean_c1))
+	print(paste('Correct_total mean: ',mean_total))
+	print(paste('Correct_c0 mean:    ',mean_c0))
+	print(paste('Correct_c1 mean:    ',mean_c1))
 }else{
 	## Make submission
 	file_train <- '../train.dat'
